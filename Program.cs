25206d43d1df0063f4a2f7d34ba771894d938f0c
Jaymes <jaylink5353@gmail.com>
2025-07-21 15:14:29 -0400

    Added printGrid function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Created by Jaylink5353
+Simple C# TicTacToe!!
+*/
+
+bool play = true;
+Console.WriteLine("Simple C# Tic Tac Toe!");
+Console.WriteLine("Made by Jaylink5353");
+
+int game = 0;
+
+while (play == true && game < +5)
+{
+    printGrid();
+    game++;
+
+}
+
+void gameInit()
+{
+    
+}
+void printGrid()
+{
+    Console.WriteLine("   a     b     c ");
+    Console.WriteLine("      |     |     ");
+    Console.WriteLine($"1  -  |  -  |  -  ");
+    Console.WriteLine("______|_____|_____");
+    Console.WriteLine("      |     |     ");
+    Console.WriteLine($"2  -  |  -  |  -  ");
+    Console.WriteLine("______|_____|_____");
+    Console.WriteLine("      |     |     ");
+    Console.WriteLine($"3  -  |  -  |  -  ");
+    Console.WriteLine("      |     |     ");
+    
+}
